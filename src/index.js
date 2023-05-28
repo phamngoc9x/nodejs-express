77ccab1f05533578a4586411930efcf35340dfc4
CoinCrawVN <m1@CoinCrawVN.local>
2023-05-28 09:02:24 +0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 // Http logger
 app.use(morgan('combined'))
@@ -18,12 +24,9 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
